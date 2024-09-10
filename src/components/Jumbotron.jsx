2afaa4d5d9fe9e5420a89b60026735670aa97887
Alex Deane <alexdeane16@gmail.com>
2024-09-10 17:31:17 -0400

    Fix: Learn more button routes to About page

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import '../css/Jumbotron.css';
 import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import '../css/Jumbotron.css';
+
 const Jumbotron = () => {
     return (
         <div className="jumbotron text-center">
             <div className="text-center my-4">
                 <Image
-                src="./images/Vernon.svg" // Update this path to the actual logo file path
-                alt="Vernon Travel Basketball Logo"
-                fluid // This makes the image responsive
-                style={{ width: '300px', height: 'auto' }} // Adjust the width for banner-like appearance
+                    src="./images/Vernon.svg" // Make sure the path is correct
+                    alt="Vernon Travel Basketball Logo"
+                    fluid // This makes the image responsive
+                    style={{ width: '300px', height: 'auto' }} // Adjust the width for banner-like appearance
                 />
             </div>
             <p className="lead">Vernon Travel Basketball is a youth basketball program in the town of Vernon, Connecticut.</p>
             <hr className="my-4" />
             <p>Tryouts and other information can be found here on our website as well as our Facebook page.</p>
             <p className="lead">
-                <a className="btn btn-primary btn-lg" href="../about" role="button">Learn more</a>
+                <Link className="btn btn-primary btn-lg" to="/about" role="button">
+                    Learn more
+                </Link> {/* Use Link from react-router-dom */}
             </p>
         </div>
     );
